45f2fc49c0cf9fb3c08ae5393fdb1f723c976ca3
Mario931025 <79239437+Mario931025@users.noreply.github.com>
2021-06-23 00:07:08 -0500

    estilos login terminados 2

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,6 +7,7 @@ import {useNavigation} from '@react-navigation/native'
 export default function LoginForm() {
     const [email, setemail] = useState("")
     const [password, setpassword] = useState("")
+    
 
     return (
         <View style={styles.container}>
@@ -18,12 +19,57 @@ export default function LoginForm() {
             }}
             />
 
-            <Input placeholder="Correo"/>
-            <Input placeholder="Contraseña"/>
-            <Button title="Entrar"/>
-            <Text>
+            <Input placeholder="Correo"
+            containerStyle={styles.input}
+            rightIcon={{
+                type:"material-community",
+                name:"eye-outline",
+                color:"#128c7e",
+                onPress : () => alert("Hola")
+            }}
+
+            leftIcon={{
+                type : "material-community",
+                name : "account-circle-outline",
+                color:"#128c7e",
+            }}
+
+            onChangeText={(text) => {
+                setemail(text);
+              }}
+              value={email}
+
+            />
+            <Input placeholder="Contraseña"
+            containerStyle={styles.input}
+            rightIcon={{
+                type:"material-community",
+                name:"eye-outline",
+                color:"#128c7e",
+                onPress : () => alert("Hola")
+            }}
+
+            leftIcon={{
+               type:"material-community",
+               name:"security",
+               color:"#128c7e",
+            }}
+
+            onChangeText={(text) => {
+                setpassword(text);
+              }}
+              secureTextEntry={true}
+              value={password}
+            />
+
+
+            <Button title="Entrar"
+            containerStyle={styles.btnentrar}
+            buttonStyle={{backgroundColor:"#25d366"}} //estilo que se aplica adentro de
+            />
+            <Text style={styles.txtcrearcuenta}>
                 ¿No tienes cuenta? 
-                <Text>
+                <Text style={styles.cuenta}>
                     Crear Cuenta
                 </Text>
             </Text>
@@ -36,10 +82,10 @@ export default function LoginForm() {
                     marginTop:20
                 }}
             />
-            <Text>O</Text>
+            <Text style={styles.txto}>O</Text>
 
-            <View>
-                <TouchableOpacity>
+            <View style={styles.btnlogin}>
+                <TouchableOpacity style={styles.btnloginsocial}>
                     <Icon 
                     size={24}
                     type="material-community"
@@ -49,7 +95,7 @@ export default function LoginForm() {
                     />
                 </TouchableOpacity>
 
-                <TouchableOpacity>
+                <TouchableOpacity style={styles.btnloginsocial}>
                     <Icon 
                     size={24}
                     type="material-community"
@@ -74,5 +120,40 @@ const styles = StyleSheet.create({
         borderTopRightRadius:50,
         alignItems:"center",// se alinenen los inputs al centro
         paddingTop:20, //par que tener un paddign de este conmponente
+    },
+    input:{
+        width:"90%",
+        marginTop:20,
+        height:50
+    },
+    btnentrar:{
+        width:"90%",
+        marginTop:20,
+
+    },
+    txtcrearcuenta: {
+        marginTop: 20,
+    },
+    cuenta: {
+        color: "#128c7e",
+        fontFamily: "Roboto",
+        fontSize: 15,
+    },
+    txto: {
+        fontWeight: "bold",
+        fontSize: 20,
+        marginTop: 20,
+        color: "#128c7e",
+      },
+    btnlogin: {
+        flexDirection: "row",  //no se aline vertital, horizontal
+        justifyContent: "space-around", //distribuye los elementos a lo largo de la pantalla
+        width: "100%", //para que agarre toda la pantlla
+      },
+    btnloginsocial: {
+        backgroundColor: "#25d366",
+        paddingHorizontal: 40,
+        paddingVertical: 10,
+        borderRadius: 5,
     }
 })
